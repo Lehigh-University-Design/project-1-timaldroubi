302d55ff4673364c7bb1b566efcbf695f687f26d
Tiana Aldroubi <tianama02@gmail.com>
2024-02-07 09:24:30 -0500

    added photos and background stickers

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function showImage(id) {
+   
+    document.getElementById(id.toString()).style.display = 'block';
+    // console.log("over")
+}
+
+function hideImage(id) {
+    document.getElementById(id.toString()).style.display = 'none';
+    // console.log("gone")
+}
